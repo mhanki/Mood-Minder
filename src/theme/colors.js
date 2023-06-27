650c76101a4cc1344b514698f23c12e8e3ab96f7
mhanki <meike.hankewicz@protonmail.com>
2023-06-27 15:47:16 +0200

    update theme

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,17 +1,20 @@
 export const colors = {
   ui: {
-    primary: "#5388D8",
-    secondary: "#485C7D",
+    primary: "#58A4B0" /* "#5388D8" */,
+    secondary: "#A9BCD0" /* "#485C7D" */,
+    tertiary: "#D8DBE2",
     disabled: "#DEDEDE",
     error: "#D0421B",
     success: "#138000",
   },
   bg: {
-    primary: "#FFFFFF",
-    secondary: "#485C7D",
+    primary: "#292F3D",
+    lighter: "#373F51",
+    secondary: "#58A4B0" /* "#485C7D" */,
+    tertiary: "#DAA49A"
   },
   text: {
-    primary: "#262626",
+    primary: "white" /* "#262626" */,
     secondary: "#757575",
     disabled: "#9C9C9C",
     inverse: "#FFFFFF",
--- src/theme/spacing.js
+++ src/theme/spacing.js
@@ -1,5 +1,6 @@
 export const space = {
   none: 0,
+  xs: 5,
   small: 8,
   medium: 16,
   big: 32,
