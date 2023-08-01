1c99bb6c87b25168896851544833eb45cb09aa29
mhanki <meike.hankewicz@protonmail.com>
2023-08-01 10:39:30 +0200

    add copy to mood graph screen

--- src/features/moodGraph/screens/MoodGraphScreen.tsx
+++ src/features/moodGraph/screens/MoodGraphScreen.tsx
@@ -15,7 +15,7 @@ export const MoodGraphScreen = () => {
   return (
     <SafeArea>
       <Text variant="heading" style={{ marginBottom: 16 }}>Everything Changes</Text>
-      <Text>Lorem ipsum dolor sit amet consectetur. Et quis vestibulum id quam diam mauris. Sit sit porttitor blandit ac donec sit. Id ut bibendum molestie ipsum id erat duis. Suspendisse quam enim justo morbi et. Dictum odio amet vestibulum ipsum velit eu. Amet dignissim proin erat sed. Ac est pellentesque molestie morbi quam nunc habitasse.</Text>
+      <Text>Nothing in life is permanent, including our moods and emotions. The essence of Cognitive Behavioral Therapy (CBT) lies in the recognition that our thoughts, feelings, and behaviours are interconnected. CBT teaches us that our moods and emotions are not fixed entities, but rather products of our thoughts and interpretations of events. By understanding that our feelings are not permanent, we gain the power to challenge negative thoughts and beliefs to find greater balance.</Text>
       { logs.length < 2 
         ? <Text style={{ marginTop: 50 }}>You haven't logged enough data yet</Text>
         : <MoodGraph logs={logs} feelings={feelings} />
